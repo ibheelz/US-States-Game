8bfcb37d4c8fecde9569b63835c5fb2ff3350007
ibheelz <ibheelz@icloud.com>
2024-04-12 23:03:56 +0100

    Added Text Input and Prompt

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+
+turtle.mainloop()
