11a24b1985cae58b6252ed266e0bbc7b703ce85c
ibheelz <ibheelz@icloud.com>
2024-05-21 19:51:09 +0100

    First Commit

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import turtle
+import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
-
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
-answer = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+data = pd.read_csv('50_states.csv')
+states = (data['state'])
+
+correct = 0
+answer = screen.textinput(title=f"{correct}/50 States Correct", prompt="What's another state's name?")
+
 
 turtle.mainloop() 
-
-Bass = 12
